47c4fe63b2a53cc8d62336d4865ace5c22010e5b
anish <anishsalgia1997@gmail.com>
2020-12-26 16:25:50 +0530

    Add: Added code for employee wage of multiple companies

--- wage.java
+++ wage.java
@@ -1,29 +1,38 @@
 public class wage
 {
-	public static final int FULL_TIME=1;
-	public static final int PART_TIME=2;
+        public static final int FULL_TIME=1;
+        public static final int PART_TIME=2;
+        public int companyCount=0;
+        public CompanyWage[] companyWageArray;						//Array for wages of a different companies
 
-	public String company;
-	public int empRatePerHr;
-	public int numOfWorkingDays;
-	public int maxWorkingHrs;
+        public EmployeeWageComputation()						//Default constructor
+        {
+                companyWageArray=new CompanyWage[4];
+        }
 
-	public EmpWageObjectOriented(String company,int empRatePerHr,int numOfWorkingDays,int maxWorkingHrs)
-	{
-		this.company=company;
-		this.empRatePerHr=empRatePerHr;
-		this.numOfWorkingDays=numOfWorkingDays;
-		this.maxWorkingHrs=maxWorkingHrs;
-	}
-	public void computeEmpWage()
-	{
-		int empHrs=0;
+        public void addCompanyWage(String company,int empRatePerHr,int numOfWorkingDays,int maxWorkingHrs)	//Add method for adding companies
+        {
+                companyWageArray[companyCount]=new CompanyWage(company,empRatePerHr,numOfWorkingDays,maxWorkingHrs);
+                companyCount++;
+        }
+
+        public void computeWage()							//Compute method for storing wages in array
+        {
+                for(int i=0;i<companyCount;i++)
+                {
+                        computeEmpWage(companyWageArray[i]);
+                }
+        }
+
+        public void computeEmpWage(CompanyWage company)					//computes employee wages
+        {
+                int empHrs=0;
                 int totalEmpWage=0;
                 int empWage=0;
                 int totalEmpHrs=0;
                 int totalWorkingDays=0;
 
-                while(totalEmpHrs<=maxWorkingHrs && totalWorkingDays<=numOfWorkingDays)
+                while(totalEmpHrs<=company.maxWorkingHrs && totalWorkingDays<=company.numOfWorkingDays)
                 {
                         totalWorkingDays++;
                         int empCheck=(int)Math.floor(Math.random()*10)%3;
@@ -43,18 +52,34 @@ public class wage
                         }
                         totalEmpHrs+=empHrs;
                 }
-                totalEmpWage=totalEmpHrs*empRatePerHr;
-                System.out.println("Company: "+company+" Total Employee Wage: "+totalEmpWage);
+                totalEmpWage=totalEmpHrs*company.empRatePerHr;
+                System.out.println("Company: "+company.company+" Total Employee Wage: "+totalEmpWage);
 
-	}
+        }
 
-	public static void main(String[] args)
-	{
-		EmpWageObjectOriented dmart=new EmpWageObjectOriented("Dmart",12,34,56);
-		EmpWageObjectOriented realiance=new EmpWageObjectOriented("realiance",22,44,16);
- 		EmpWageObjectOriented deloitte=new EmpWageObjectOriented("Deloitte",62,44,26);
-		dmart.computeEmpWage();
-		realiance.computeEmpWage();
-		deloitte.computeEmpWage();			
-	}
+        public static void main(String[] args)						//main method
+        {
+                EmployeeWageComputation builder=new EmployeeWageComputation();
+                builder.addCompanyWage("Dmart",12,34,56);
+                builder.addCompanyWage("Reliance",22,14,26);
+		builder.addCompanyWage("Deloitte",62,44,26);
+                builder.computeWage();
+        }
+}
+
+class CompanyWage									//class for variables of a company
+{
+        public String company;
+        public int empRatePerHr;
+        public int numOfWorkingDays;
+        public int maxWorkingHrs;
+
+        public CompanyWage(String company,int empRatePerHr,int numOfWorkingDays,int maxWorkingHrs)	//Parameterized constructor
+        {
+                this.company=company;
+                this.empRatePerHr=empRatePerHr;
+                this.numOfWorkingDays=numOfWorkingDays;
+                this.maxWorkingHrs=maxWorkingHrs;
+        }
+
 }
